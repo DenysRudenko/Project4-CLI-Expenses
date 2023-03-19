8d195e1fdb13330e1f477a68938bf56bb91b3191
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-03-19 22:48:02 +0000

    wrote a function that shows and hide error message in registration form if user input the symbols

--- django_todo/static/js/register.js
+++ django_todo/static/js/register.js
@@ -1,1 +1,29 @@
-console.log("hello");
+const usernameField = document.querySelector('#usernameField');
+const feedBackArea = document.querySelector('.invalid_feedback');
+
+usernameField.addEventListener('keyup', (e) => {
+    console.log("test");
+    const usernameVal = e.target.value;
+
+    usernameField.classList.remove('is-invalid');
+    feedBackArea.style.display = 'none';
+
+
+
+    // not allowing the user input symbols in username area and showing the erorr if he does it
+    if (usernameVal.length > 0) {
+        fetch('/authentication/validate-username', {
+        body: JSON.stringify({ username: usernameVal }),
+        method: 'POST',
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data", data);
+            if (data.username_error) {
+                usernameField.classList.add('is-invalid');
+                feedBackArea.style.display = 'block';
+                feedBackArea.innerHTML = `<p>${data.username_error}</p>`
+            }
+        });
+    }
+});
