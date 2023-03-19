8dbcc5a644c57bc3f102bd2681cf984b2cc99503
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-03-19 20:02:21 +0000

    Added one more option to if statement for class usernameValidationView in view.py authentication folder

--- authentication/views.py
+++ authentication/views.py
@@ -6,10 +6,12 @@ from django.http import JsonResponse
 
 class UsernameValidationView(View):
     def post(self, request):
-        data=json.loads(request.body)
-        username=data['username']
+        data = json.loads(request.body)
+        username = data['username']
         if not str(username).isalnum():
-            return JsonResponse({'username_error':' username should only contain alph characters'})
+            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
         return JsonResponse({'username_valid': True})
     
 class RegistrationView(View):
