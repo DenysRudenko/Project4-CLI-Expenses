14e37f010e7ab20ac0187242496cad13d7b29a9e
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-03-21 20:17:38 +0000

    Created if statement for user login in view.py

--- authentication/views.py
+++ authentication/views.py
@@ -129,3 +129,25 @@ class VerificationView(View):
 class LoginView(View):
     def get(self, request):
          return render(request, 'authentication/login.html')
+     
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        if username and password:
+            user=auth.authenticate(username=username, password= password)
+            
+            if user:
+                if user.is_active:
+                    auth.login(request, user)
+                    messages.success(request, 'Welcome, ' + 
+                                     user.username+ ' You are now logged in')
+            
+                messages.error(request, 'Account is not active, please check your email')
+                return render(request, 'authentication/login.html')
+
+            messages.error(request, 'Invalid credentials, try again')
+            return render(request, 'authentication/login.html')
+        
+        messages.error(request, 'Please fill all fields')
+        return render(request, 'authentication/login.html')
