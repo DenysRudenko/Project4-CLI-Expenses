f6c590d8b1745d35750a85fec48a4dfdad2d14a7
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-03-21 20:41:41 +0000

    Created @login_required rule in expenses/view.py to protect the root if user want to press back button in browser

--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
+# Protect the root
+@login_required(login_url='/authentication/login')
 # taking requests and taking back responses
 def index(request):
     return render(request, 'expenses/index.html')
