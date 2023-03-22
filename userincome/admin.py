d81382b37e156482a3f9bdafd8dedc0890f625af
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-03-22 23:34:17 +0000

    Styled admin panel and added more description fields for content in our user incomes and user expenses

--- userincome/admin.py
+++ userincome/admin.py
@@ -3,5 +3,8 @@ from .models import UserIncome, Source
 
 # Register your models here.
 
-admin.site.register(UserIncome)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display= ('amount','description','owner','source', 'date')
+
+admin.site.register(UserIncome, IncomeAdmin)
 admin.site.register(Source)
