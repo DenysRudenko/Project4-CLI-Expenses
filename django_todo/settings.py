c12518f20c86d34110085aa0f03ae63199777483
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-03-22 19:15:58 +0000

    Done migrations with our new app userincome

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'expenses',
-    'userpreferences'
+    'userpreferences',
+    'userincome'
 ]
 
 MIDDLEWARE = [
