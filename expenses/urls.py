3cf78beda348b8109cbde03be72f84c31d4a5ec9
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-03-23 12:44:32 +0000

    Created chartjs model in stats.html

--- expenses/urls.py
+++ expenses/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('add-expense', views.add_expense, name="add-expenses"),
     path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
     path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
-    path('search-expenses', csrf_exempt(views.search_expenses), name="seach_expenses")
+    path('search-expenses', csrf_exempt(views.search_expenses), name="seach_expenses"),
+    path('expense_category_summary', views.expense_category_summary, name='views.expense_category_summary'),
+    path('stats', views.stats_view , name='stats')
 ]
