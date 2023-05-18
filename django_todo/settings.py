c2b7a8de7811ea187713a8246e6e7cc12a4ddfa3
Denys Rudenko <heartbreathing19951014@gmail.com>
2023-05-18 17:11:23 +0100

    Changed dubug to false

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = 'DEV' in os.environ
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
